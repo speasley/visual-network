fe92e5c2f205692dc612d2e88516598286c59b9e
Stephen Peasley <speasley@users.noreply.github.com>
2024-05-05 09:40:06 -0600

    build: init with vis.js

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+import { DataSet, Network } from 'vis-network/standalone/esm/vis-network'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      nodes: new DataSet([]),
+      edges: new DataSet([])
+    }
+  },
+  mounted() {
+    const container = this.$refs.networkContainer
+    const data = {
+      nodes: this.nodes,
+      edges: this.edges
+    }
+    new Network(container, data, {})
+  }
+}
+</script>
+
+<template>
+  <main ref="networkContainer" class="network-container"></main>
+</template>
+
+<style scoped>
+.network-container {
+  height: 350px;
+  width: 100%;
+}
+</style>
